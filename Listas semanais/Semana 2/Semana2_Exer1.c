f10576de9295202c18ecb687ea75c826c74715d1
Thata <71095424+thatarocket@users.noreply.github.com>
2021-08-30 23:50:00 -0300

    Delete Listas semanais directory

--- Listas semanais/Semana 2/Semana2_Exer1.c
+++
@@ -1,9 +0,0 @@
-typedef struct aux {
-  int id;
-  struct aux* mae;
-  struct aux* pai;
-} PESSOA;
-
-int main() {
-  return 0;
-}
--- Listas semanais/Semana 5/Semana5_Exer1.c
+++
@@ -1,9 +0,0 @@
-void exibirPilhaInvertida(PILHA* p){
-
-  printf("Pilha (da base para o topo): \" ");
-  for (int i = 0; i <= p->topo; i++) {
-    printf(" %i ", p->A[i].chave);
-  }
-  printf("\"\n");
-  
-}
